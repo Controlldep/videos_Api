6c0e33a663065db3701b98f9a688c253f050bdd5
Klopov Vladimir <faliingsky@mail.ru>
2025-08-19 09:10:42 +0400

    create base videos Api

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { setupApp } from "./setup-app";
+
+
+export const app = express();
+setupApp(app);
+
+
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5001;
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}`);
+    });
+}
+
+export default app;
+
+
